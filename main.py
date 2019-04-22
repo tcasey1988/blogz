380384798129801d67ba7d20157b740c0a69943d
Tom Casey <tcasey1988@gmail.com>
2019-04-22 01:10:04 -0500

    Finish display posts

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
@@ -17,25 +16,40 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
+@app.route('/newpost')
+def new_post():
+    return render_template('newpost.html', title="Add a New Entry")
 
-@app.route('/blog', methods=['POST'])
-def blog():
-    return render_template("blog.html")
+@app.route('/blog', methods=['POST', 'GET'])
+def index():
+    blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs, title="Build a Blog")
 
 @app.route('/newpost', methods=['POST', 'GET'])
-def new_post():
+def submit_post():
+    title_name = request.form['title']
+    body_name = request.form['body']
 
-    if request.method == "POST":
-        blog_title = request.form["title"]
-        new_title = Blog(blog_title)
-        db.session.add(new_title)
-        blog_body = request.form["body"]
-        new_body = Blog(blog_body)
-        db.session.add(new_body)
+    title_error = ""
+    body_error = ""
+
+    if title_name == "":
+        title_error = "Not a valid title"
+    else:
+        title_name = title_name
+
+    if body_name == "":
+        body_error = "Not a valid body"
+    else:
+        body_name = body_name
+
+    if not title_error and not body_error:
+        new_entry = Blog(title_name, body_name)
+        db.session.add(new_entry)  
         db.session.commit()
-
-    return render_template("newpost.html")
-
-
+        return redirect("/blog")
+    else:
+        return render_template("newpost.html", title_error=title_error, body_error=body_error, title="Build a Blog")
+    
 if __name__ == '__main__':
     app.run()
