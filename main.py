3c48ce9481b74f8629420ac011e1d406d054eb32
Tom Casey <tcasey1988@gmail.com>
2019-04-22 17:27:22 -0500

    finished cases

--- main.py
+++ main.py
@@ -16,30 +16,35 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route('/newpost')
-def new_post():
-    return render_template('newpost.html', title="Add a New Entry")
-
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
+    if request.args.get('id'):
+        blog_id = request.args.get('id')
+        post = Blog.query.filter_by(id=blog_id).first()
+        return render_template('viewpost.html', title='Post', post=post)
+        
     blogs = Blog.query.all()
-    return render_template('blog.html', blogs=blogs, title="Build a Blog")
+    return render_template('blog.html', blogs=blogs, title='Build a Blog')
+
+@app.route('/newpost')
+def new_post():
+    return render_template('newpost.html', title='Add a New Entry')
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def submit_post():
     title_name = request.form['title']
     body_name = request.form['body']
 
-    title_error = ""
-    body_error = ""
+    title_error = ''
+    body_error = ''
 
-    if title_name == "":
-        title_error = "Not a valid title"
+    if title_name == '':
+        title_error = 'Not a valid title'
     else:
         title_name = title_name
 
-    if body_name == "":
-        body_error = "Not a valid body"
+    if body_name == '':
+        body_error = 'Not a valid body'
     else:
         body_name = body_name
 
@@ -47,9 +52,12 @@ def submit_post():
         new_entry = Blog(title_name, body_name)
         db.session.add(new_entry)  
         db.session.commit()
-        return redirect("/blog")
+        post_id = Blog.query.filter_by(title=title_name).first().id
+        return redirect('/blog?id={0}'.format(post_id))
     else:
-        return render_template("newpost.html", title_error=title_error, body_error=body_error, title="Build a Blog")
-    
+        return render_template('newpost.html', title_error=title_error, body_error=body_error, title='Build a Blog')
+
+
+
 if __name__ == '__main__':
     app.run()
