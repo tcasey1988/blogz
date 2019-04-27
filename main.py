d2b0b8607033c1c90d66586a2ba2a468de2a9d17
Tom Casey <tcasey1988@gmail.com>
2019-04-26 19:10:06 -0500

    Finish signup/register pages. Made login/logout features

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,18 +6,38 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'secretkey'
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blog = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register', 'signup', 'list_blogs']
+    if request.endpoint not in allowed_routes and 'user' not in session:
+        return redirect('/login')
 
 @app.route('/blog', methods=['POST', 'GET'])
-def index():
+def list_blogs():
     if request.args.get('id'):
         blog_id = request.args.get('id')
         post = Blog.query.filter_by(id=blog_id).first()
@@ -40,13 +60,9 @@ def submit_post():
 
     if title_name == '':
         title_error = 'Not a valid title'
-    else:
-        title_name = title_name
 
     if body_name == '':
         body_error = 'Not a valid body'
-    else:
-        body_name = body_name
 
     if not title_error and not body_error:
         new_entry = Blog(title_name, body_name)
@@ -57,7 +73,70 @@ def submit_post():
     else:
         return render_template('newpost.html', title_error=title_error, body_error=body_error, title='Build a Blog')
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['user'] = user.email
+            flash('Logged In')
+            return redirect('/newpost')
+        else:
+            if not user:
+                flash('Incorrect username')
+                return redirect('/login')
+            elif user.password != password:
+                flash('Incorrect password')
+                return redirect('/login')
 
+    return render_template('login.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if email == "" or password == "" or verify == "":
+            flash('One or more fields invalid')
+            return redirect('/signup')
+
+        if password != verify:
+            flash('Passwords do not match')
+            return redirect('/signup')
+
+        if len(email) < 3:
+            flash('Invalid email')
+            return redirect('/signup')
+
+        if len(password) < 3:
+            flash('Invalid password')
+            return redirect('/signup')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('User already exists')
+            return redirect('/signup')
+        
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        session['user'] = user.email
+        return redirect('/newpost')
+    else:
+        return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['user']
+    return redirect('/login')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
