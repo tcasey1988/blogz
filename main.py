ad55d1e94e542b5f0c6bc8b8c95e55437671de8c
Tom Casey <tcasey1988@gmail.com>
2019-04-29 18:55:39 -0500

    Added MVC and hashing

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
 from flask import Flask, request, redirect, render_template, session, flash
-from flask_sqlalchemy import SQLAlchemy
+from app import app, db
+from models import User, Blog
+from hashutils import check_pw_hash
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
-app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
 app.secret_key = 'secretkey'
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(120))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, title, body, owner):
-        self.title = title
-        self.body = body
-        self.owner = owner
-
-class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120))
-    password = db.Column(db.String(120))
-    blog = db.relationship('Blog', backref='owner')
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
 
 @app.before_request
 def require_login():
@@ -83,7 +58,7 @@ def login():
         user = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=user).first()
-        if user and user.password == password:
+        if user and check_pw_hash(password, user.pw_hash):
             session['user'] = user.username
             flash('Logged In')
             return redirect('/newpost')
@@ -133,15 +108,15 @@ def signup():
     else:
         return render_template('signup.html')
 
-@app.route('/logout')
-def logout():
-    del session['user']
-    return redirect('/blog')
-
 @app.route('/', methods=['POST', 'GET'])
 def home():
     users = User.query.all()
     return render_template('index.html', title="Blogz", users=users)
+
+@app.route('/logout')
+def logout():
+    del session['user']
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,26 @@
+from app import db
+from hashutils import make_pw_hash
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, owner):
+        self.title = title
+        self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    pw_hash = db.Column(db.String(120))
+    blog = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.pw_hash = make_pw_hash(password)
